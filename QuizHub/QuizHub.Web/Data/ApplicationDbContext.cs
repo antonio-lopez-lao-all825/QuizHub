6d6b18e80ba40237ab8949fa324c461840fb9a40
antonio-lopez-lao-all825 <all825@inlumine.ual.es>
2024-12-22 18:55:09 +0100

    feat: vista Preguntas (no terminada)

--- QuizHub/QuizHub.Web/Data/ApplicationDbContext.cs
+++ QuizHub/QuizHub.Web/Data/ApplicationDbContext.cs
@@ -12,6 +12,5 @@ namespace QuizHub.Web.Data
         public DbSet<Asignatura> asignaturas { get; set; }
         public DbSet<AsinaturasUsuario> asignaturasUsuario { get; set; }
         public DbSet<Puntuacion> puntuacion { get; set; }
-
     }
 }
--- QuizHub/QuizHub.Web/Data/Respuesta.cs
+++ QuizHub/QuizHub.Web/Data/Respuesta.cs
@@ -2,10 +2,10 @@
 {
     public class Respuesta
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int? IdPregunta { get; set; }
-        public string Texto { get; set; }
-        public bool Correcta { get; set; }
-        public int Estado { get; set; }
+        public string? Texto { get; set; }
+        public bool? Correcta { get; set; }
+        public int? Estado { get; set; }
     }
 }
