d77af05e7f60b7ddd204820dd73d49c9ee26747e
Ayoub Fehri Boulaadas <afb356@inlumine.ual.es>
2025-01-14 18:44:08 +0100

    Migracion del backend existente

--- QuizHub/QuizHub.Web/Migrations/20250114174248_Backend_2025_01_14.Designer.cs
+++ QuizHub/QuizHub.Web/Migrations/20250114174248_Backend_2025_01_14.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizHub.Web.Data;
 
@@ -10,9 +11,11 @@ using QuizHub.Web.Data;
 namespace QuizHub.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250114174248_Backend_2025_01_14")]
+    partial class Backend_2025_01_14
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
